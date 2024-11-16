21b814d4cc93dbdab3f4a2e225f20ad2f480a60b
putitu8 <putitu82@gmail.com>
2024-11-16 12:53:57 +0800

    test

---
+++ test.py
@@ -0,0 +1,140 @@
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader, random_split
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+def make_train_step_fn(model, loss_fn, optimizer):
+    def train_step(x, y):
+        model.train()
+        yhat = model(x)
+        loss = loss_fn(yhat, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        return loss.item()
+    return train_step
+
+def make_val_step_fn(model, loss_fn):
+    def val_step(x, y):
+        model.eval()
+        yhat = model(x)
+        loss = loss_fn(yhat, y)
+        return loss.item()
+    return val_step
+
+def mini_batch(device, data_loader, step_fn):
+    mini_batch_losses = []
+    for x_batch, y_batch in data_loader:
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+        
+        mini_batch_loss = step_fn(x_batch, y_batch)
+        mini_batch_losses.append(mini_batch_loss)
+        
+    loss = np.mean(mini_batch_losses)
+    return loss
+
+# Data Generation
+true_b = 1
+true_w = 2
+N = 100
+
+np.random.seed(42)
+x = np.random.rand(N, 1)
+y = true_b + true_w * x + (.1 * np.random.randn(N, 1))
+
+# Shuffles the indices
+idx = np.arange(N)
+np.random.shuffle(idx)
+
+# Uses first 80 random indices for train
+train_idx = idx[:int(N*.8)]
+# Uses the remaining indices for validation
+val_idx = idx[int(N*.8):]
+
+# Generates train and validation sets
+x_train, y_train = x[train_idx], y[train_idx]
+x_val, y_val = x[val_idx], y[val_idx]
+
+
+# Data Preparation
+
+torch.manual_seed(13)
+
+# Builds tensors from numpy arrays BEFORE split
+x_tensor = torch.from_numpy(x).float()
+y_tensor = torch.from_numpy(y).float()
+
+# Builds dataset containing ALL data points
+dataset = TensorDataset(x_tensor, y_tensor)
+
+# Performs the split
+ratio = .8
+n_total = len(dataset)
+n_train = int(n_total * ratio)
+n_val = n_total - n_train
+
+train_data, val_data = random_split(dataset, [n_train, n_val])
+
+# Builds a loader of each set
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+val_loader = DataLoader(dataset=val_data, batch_size=16)
+
+
+# Model Configuration
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Sets learning rate - this is "eta" ~ the "n" like Greek letter
+lr = 0.1
+
+torch.manual_seed(42)
+# Now we can create a model and send it at once to the device
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+# Defines a SGD optimizer to update the parameters (now retrieved directly from the model)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# Defines a MSE loss function
+loss_fn = nn.MSELoss(reduction='mean')
+
+# Creates the train_step function for our model, loss function and optimizer
+train_step_fn = make_train_step_fn(model, loss_fn, optimizer)
+
+# Creates the val_step function for our model and loss function
+val_step_fn = make_val_step_fn(model, loss_fn)
+
+# Creates a Summary Writer to interface with TensorBoard
+writer = SummaryWriter('runs/simple_linear_regression')
+
+# Fetches a single mini-batch so we can use add_graph
+x_sample, y_sample = next(iter(train_loader))
+writer.add_graph(model, x_sample.to(device))
+
+# Model Training
+
+# Defines number of epochs
+n_epochs = 200
+
+losses = []
+val_losses = []
+
+for epoch in range(n_epochs):
+    # inner loop
+    loss = mini_batch(device, train_loader, train_step_fn)
+    losses.append(loss)
+    
+    # VALIDATION
+    # no gradients in validation!
+    with torch.no_grad():
+        val_loss = mini_batch(device, val_loader, val_step_fn)
+        val_losses.append(val_loss)
+    
+    # Records both losses for each epoch under the main tag "loss"
+    writer.add_scalars(main_tag='loss',
+                       tag_scalar_dict={'training': loss, 'validation': val_loss},
+                       global_step=epoch)
+
+# Closes the writer
+writer.close()
