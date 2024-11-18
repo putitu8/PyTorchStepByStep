d0fe92498c144401cd2f1223832614308248f6d5
Jack <putitu82@gmail.com>
2024-11-18 16:54:29 +0800

    chp3 finished

--- test_chapter3.py
+++ test_chapter3.py
@@ -51,110 +51,12 @@ val_loader = DataLoader(dataset=val_dataset, batch_size=16)
 def odds_ratio(prob):
     return prob / (1 - prob)
 
-p = .75
-q = 1 - p
-odds_ratio(p), odds_ratio(q)
-
-fig = figure2(p)
-
 def log_odds_ratio(prob):
     return np.log(odds_ratio(prob))
-
-p = .75
-q = 1 - p
-log_odds_ratio(p), log_odds_ratio(q)
-
-fig = figure3(p)
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-p = .75
-q = 1 - p
-sigmoid(log_odds_ratio(p)), sigmoid(log_odds_ratio(q))
-
-torch.sigmoid(torch.tensor(1.0986)), torch.sigmoid(torch.tensor(-1.0986))
-
-fig = figure4(p)
-
-torch.manual_seed(42)
-model1 = nn.Sequential()
-model1.add_module('linear', nn.Linear(2, 1))
-model1.add_module('sigmoid', nn.Sigmoid())
-print(model1.state_dict())
-
-dummy_labels = torch.tensor([1.0, 0.0])
-dummy_predictions = torch.tensor([.9, .2])
-
-# Positive class (labels == 1)
-positive_pred = dummy_predictions[dummy_labels == 1]
-first_summation = torch.log(positive_pred).sum()
-# Negative class (labels == 0)
-negative_pred = dummy_predictions[dummy_labels == 0]
-second_summation = torch.log(1 - negative_pred).sum()
-# n_total = n_pos + n_neg
-n_total = dummy_labels.size(0)
-
-loss = -(first_summation + second_summation) / n_total
-loss
-
-summation = torch.sum(
-    dummy_labels * torch.log(dummy_predictions) +
-    (1 - dummy_labels) * torch.log(1 - dummy_predictions)
-)
-loss = -summation / n_total
-loss
-
-loss_fn = nn.BCELoss(reduction='mean')
-
-loss_fn
-
-dummy_labels = torch.tensor([1.0, 0.0])
-dummy_predictions = torch.tensor([.9, .2])
-
-# RIGHT
-right_loss = loss_fn(dummy_predictions, dummy_labels)
-
-# WRONG
-wrong_loss = loss_fn(dummy_labels, dummy_predictions)
-
-print(right_loss, wrong_loss)
-
-loss_fn_logits = nn.BCEWithLogitsLoss(reduction='mean')
-
-loss_fn_logits
-
-logit1 = log_odds_ratio(.9)
-logit2 = log_odds_ratio(.2)
-
-dummy_labels = torch.tensor([1.0, 0.0])
-dummy_logits = torch.tensor([logit1, logit2])
-
-print(dummy_logits)
-
-loss = loss_fn_logits(dummy_logits, dummy_labels)
-loss
-
-dummy_imb_labels = torch.tensor([1.0, 0.0, 0.0, 0.0])
-dummy_imb_logits = torch.tensor([logit1, logit2, logit2, logit2])
-
-n_neg = (dummy_imb_labels == 0).sum().float()
-n_pos = (dummy_imb_labels == 1).sum().float()
-
-pos_weight = (n_neg / n_pos).view(1,)
-pos_weight
-
-loss_fn_imb = nn.BCEWithLogitsLoss(reduction='mean', pos_weight=pos_weight)
-
-loss = loss_fn_imb(dummy_imb_logits, dummy_imb_labels)
-loss
-
-loss_fn_imb_sum = nn.BCEWithLogitsLoss(reduction='sum', pos_weight=pos_weight)
-
-loss = loss_fn_imb_sum(dummy_imb_logits, dummy_imb_labels)
-
-loss = loss / (pos_weight * n_pos + n_neg)
-loss
 
 # Sets learning rate - this is "eta" ~ the "n" like Greek letter
 lr = 0.1
@@ -176,47 +78,29 @@ sbs.set_loaders(train_loader, val_loader)
 sbs.train(n_epochs)
 
 fig = sbs.plot_losses()
+plt.figure(fig)
+plt.show()
+
 print(model.state_dict())
 
-predictions = sbs.predict(x_train_tensor[:4])
-predictions
-
-probabilities = sigmoid(predictions)
-probabilities
-
-classes = (predictions >= 0).astype(int)
-classes
-
-fig = figure7(X_train, y_train, sbs.model, sbs.device)
-
-fig = figure7(X_val, y_val, sbs.model, sbs.device)
-
-x = np.array([-2.8, -2.2, -1.8, -1.3, -.4, 0.3, 0.6, 1.3, 1.9, 2.5])
-y = np.array([0., 0., 0., 0., 1., 1., 1., 0., 0., 0.])
-
-fig = one_dimension(x, y)
-
-fig = two_dimensions(x, y)
-
-model = nn.Sequential()
-model.add_module('hidden', nn.Linear(2, 10))
-model.add_module('activation', nn.ReLU())
-model.add_module('output', nn.Linear(10, 1))
-model.add_module('sigmoid', nn.Sigmoid())
-
-loss_fn = nn.BCELoss()
 
 logits_val = sbs.predict(X_val)
+print(logits_val.squeeze())
 probabilities_val = sigmoid(logits_val).squeeze()
-threshold = 0.5
+print(probabilities_val)
 
-fig = figure9(X_val, y_val, sbs.model, sbs.device, probabilities_val, threshold)
-fig = figure10(y_val, probabilities_val, threshold, 0.04, False)
 
-fig = figure10(y_val, probabilities_val, threshold, 0.04, True)
-
+#标准阀值，50%
 cm_thresh50 = confusion_matrix(y_val, (probabilities_val >= .5))
-cm_thresh50
+print(cm_thresh50)
+
+#降低了阈值，所有样本都被预测为正类
+cm_thresh0 = confusion_matrix(y_val, (probabilities_val >= 0))
+print(cm_thresh0)
+
+#提高了阈值，所有样本都被预测为负类
+cm_thresh100 = confusion_matrix(y_val, (probabilities_val >= 1))
+print(cm_thresh100)
 
 def split_cm(cm):
     # Actual negatives go in the top row, 
@@ -245,8 +129,6 @@ def tpr_fpr(cm):
     
     return tpr, fpr
 
-tpr_fpr(cm_thresh50)
-
 def precision_recall(cm):
     tn, fp, fn, tp = split_cm(cm)
     
@@ -255,8 +137,18 @@ def precision_recall(cm):
     
     return precision, recall
 
-precision_recall(cm_thresh50)
 
-fig = eval_curves_from_probs(y_val, probabilities_val, [.5], annot=True)
+threshs = np.linspace(0.,1,11)
+fig = figure17(y_val, probabilities_val, threshs)
+plt.figure(fig)
+plt.show()
 
-fig = figure9(X_val, y_val, sbs.model, sbs.device, probabilities_val, threshold=0.3, shift=0.04, annot=True)
+fpr, tpr, thresholds1 = roc_curve(y_val, probabilities_val)
+prec, rec, thresholds2 = precision_recall_curve(y_val, probabilities_val)
+fig = eval_curves(fpr, tpr, rec, prec, thresholds1, thresholds2, line=True)
+plt.figure(fig)
+plt.show()
+
+auroc = auc(fpr, tpr)
+aupr = auc(rec, prec)
+print(auroc, aupr)
